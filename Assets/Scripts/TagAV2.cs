0a56a9d0d788fa9f092e89724659c90f16fd5888
Eduardo Blank Dias Moutinho <eduardo_blank2@hotmail.com>
2017-09-15 11:10:11 -0300

    AV2

---
+++ Assets/Scripts/TagAV2.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+   
+public class TagAV2 : MonoBehaviour {
+
+    [TagSelector]
+    public string tagFilter = "";
+    
+}
